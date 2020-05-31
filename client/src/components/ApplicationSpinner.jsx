4ed507efe60356b5b3e5acbe30618963cef863ab
Amin Mohammed-Coleman <amin@digitalpatterns.io>
2020-05-31 16:48:41 +0100

    spinner

--- client/src/components/ApplicationSpinner.jsx
+++ client/src/components/ApplicationSpinner.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
-
+import styled from 'styled-components';
 
 const ApplicationSpinner = ({ translationKey, args }) => {
   const { t } = useTranslation();
@@ -10,19 +10,48 @@ const ApplicationSpinner = ({ translationKey, args }) => {
     <>
       <div className="govuk-width-container">
         <div className="govuk-grid-row">
-          <div className="govuk-grid-column-full" />
-        </div>
-      </div>
-      <div>
-        <div className="govuk-grid-row">
           <div className="govuk-grid-column-full">
-            {args ? t(translationKey, args) : t(translationKey)}
+            <SpinnerContainer>
+              <Spinner />
+              <SpinnerText>{args ? t(translationKey, args) : t(translationKey)}</SpinnerText>
+            </SpinnerContainer>
           </div>
         </div>
       </div>
     </>
   );
 };
+
+
+const SpinnerContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const Spinner = styled.div`
+
+  border: 12px solid #DEE0E2;
+  border-radius: 50%;
+  border-top-color: #005EA5;
+  width: 80px;
+  height: 80px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+  @-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+`;
+
+const SpinnerText = styled.div`
+  line-height: 2;
+  text-align: center;
+`;
 
 ApplicationSpinner.defaultProps = {
   translationKey: 'loading',
